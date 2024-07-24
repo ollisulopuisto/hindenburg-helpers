8fc7de01111a5e29deb610a3f2ee0f3788229911
Olli Sulopuisto <dst@iki.fi>
2024-07-24 23:58:10 +0300

    added a tool for merging projects that share a lot of .wav files

---
+++ reorder-hindenburg-subdirectories.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+if [ $# -eq 0 ] ; then
+    echo "Please provide a directory path as an argument."
+    exit
+fi
+
+mainDir=$1
+mkdir -p "$mainDir/Combined_WAVs"
+
+find "$mainDir" -type d -print0 | while read -rd '' subdir; do
+    subDirBase=$(basename "$subdir")
+    
+    find "$subdir" -name "*.wav" -print0 | while read -rd '' wavFile; do
+        wavBase=$(basename "$wavFile")
+        
+        if [[ -e "$mainDir/Combined_WAVs/$wavBase" ]]; then
+            if [[ $(stat -f%z "$mainDir/Combined_WAVs/$wavBase") -ne $(stat -f%z "$wavFile") ]]; then
+                mv -- "$wavFile" "$mainDir/Combined_WAVs/${wavBase%.*}_$subDirBase.wav"
+            fi
+        else
+            mv -- "$wavFile" "$mainDir/Combined_WAVs/"
+        fi
+    done
+done
+
+# All .wav files should be moved by now. Modify XML .nhsx files.
+find "$mainDir" -name "*.nhsx" -print0 | while read -rd '' xmlFile; do
+    # Replace Location attribute value in AudioPool tag with new directory.
+    # Using '#' as separator because file paths contains '/'.
+    sed -i '' -e 's#<AudioPool Path="[^"]*" Location="[^"]*"#<AudioPool Path="'"$mainDir"'/Combined_WAVs" Location="'"$mainDir"'/Combined_WAVs"#g' "$xmlFile"
+done
